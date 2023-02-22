c272432afedb5370f2e56d0b46e3fd861797c969
Kaushal Patel <mr.kaushal.p@gmail.com>
2023-02-22 14:00:10 +0530

    main function created

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+
+import cv2
 # Streamlit options menu
 from streamlit_option_menu import option_menu
 
@@ -20,32 +22,54 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded", # collapsed
 )
-# Sidebar menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Menu",
-        options=["Home", "Stock Prediction", "Other", "Settings","Contact"],
-        icons=["house", "book", "back", "wrench","envelope"],
-        menu_icon="cast",
-        default_index=0,
-        orientation="vertical", # horizontal
-    )
 
-# Home page
-if selected == "Home":
-    home()
+def main():
+    # Sidebar menu
+    with st.sidebar:
+        selected = option_menu(
+            menu_title="Menu",
+            options=["Home", "Stock Prediction", "Other", "Settings","Contact"],
+            icons=["house", "book", "back", "wrench","envelope"],
+            menu_icon="cast",
+            default_index=0,
+            orientation="vertical", # horizontal
+        )
 
-# Stock Prediction page
-elif selected == "Stock Prediction":
-    stock_predict()
+    # Home page
+    if selected == "Home":
+        home()
 
-elif selected == "Settings":
-    settings()
+    # Stock Prediction page
+    elif selected == "Stock Prediction":
+        stock_predict()
 
-# Other page
-elif selected == "Other":
-    st.title("Other")
-    st.write("Getting continous data of stock")
+    elif selected == "Settings":
+        settings()
 
-elif selected == "Contact":
-    contact()
+    # Other page
+    elif selected == "Other":
+        st.title("Other")
+        st.write("Getting continous data of stock")
+
+        img_file_buffer = st.camera_input("Take a picture")
+
+        if img_file_buffer is not None:
+            # To read image file buffer with OpenCV:
+            bytes_data = img_file_buffer.getvalue()
+            cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
+            # Check the type of cv2_img:
+            # Should output: <class 'numpy.ndarray'>
+            st.write(type(cv2_img))
+
+            # Check the shape of cv2_img:
+            # Should output shape: (height, width, channels)
+            st.write(cv2_img.shape)
+
+
+    elif selected == "Contact":
+        contact()
+
+# Run the main function
+if __name__ == "__main__":
+    main()
