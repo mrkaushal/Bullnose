2112b2262889fc879a8abaddfc747e2b0b924864
Kaushal Patel <mr.kaushal.p@gmail.com>
2023-02-24 18:40:04 +0530

    Working on AngelOne API

--- private_pages/stock_predict.py
+++ private_pages/stock_predict.py
@@ -1,29 +1,43 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import json
+
+# Streamlit echarts
+from streamlit_echarts import st_echarts
+from streamlit_echarts import JsCode
 
 def stock_predict():
   st.title("Stock Prediction")
   st.write("Welcome to the stock prediction page")
 
+  uploaded_file = st.file_uploader("Choose a file", type="csv")
+  if uploaded_file is not None:
+    csv_read = pd.read_csv(uploaded_file, parse_dates=True, index_col=0)
+    # st.dataframe(df, use_container_width=True)  # Same as st.write(df)
+
+    df = pd.DataFrame(
+      columns=["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"],
+      data=csv_read,
+    )
+
+    st.dataframe(df, use_container_width=True)  # Same as st.write(df)
+
+    # convert date column to text
+
+    st.line_chart(df[['Open', 'High', 'Close']])
+    st.empty()
+    st.bar_chart()
+
   option = st.selectbox(
   'How would you like to be contacted?',
   ('RIL', 'LICI', 'SUNPHARMA'))
 
-
   if option == 'RIL':
-    uploaded_file = st.file_uploader("Choose a file", type="csv")
-    if uploaded_file is not None:
-      csv_read = pd.read_csv(uploaded_file, parse_dates=True, index_col=0)
-      # st.dataframe(df, use_container_width=True)  # Same as st.write(df)
+    st.write("RIL")
 
-      df = pd.DataFrame(
-        columns=["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"],
-        data=csv_read,
-      )
-
-      st.dataframe(df, use_container_width=True)  # Same as st.write(df)
-
-      # convert date column to text
-
-      st.line_chart(df[['Open', 'High', 'Close']])
+  elif option == 'LICI':
+    with open("stock-prices.json") as f:
+      raw_data = json.load(f)
+      print(raw_data)
+      
--- public_pages/home.py
+++ public_pages/home.py
@@ -1,8 +1,64 @@
 import streamlit as st
 from PIL import Image
+from streamlit_echarts import st_echarts
+import json
+from streamlit_echarts import JsCode
+import time
+# Streamlit Elements
+from streamlit_elements import elements, dashboard, mui, editor, media, lazy, sync, nivo
 
-logo = Image.open("media/favicon.ico")
+
 def home():
-    st.title("Dashboard")
-    st.write("Welcome to the home Page")
-    st.image(logo, width=100, caption="Bullnose")
+    with elements("html_container"):
+        # Html Container
+        st.markdown(
+            """
+            <div style="background-color:#f5f5f5; padding: 10px; border-radius: 10px;">
+                <h1 style="color: #000000; text-align: center;">Welcome to the BullNose - Stock Analysis</h1>
+                <p style="color: #000000; text-align: center;">This is a sample dashboard</p>
+                <img src="https://media.giphy.com/media/3o7TKsQ8UQJhIYq6WU/giphy.gif" alt="BullNose" style="width:100%;height:100%;">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+    # with elements("dashboard"):
+
+    #     # First, build a default layout for every element you want to include in your dashboard
+    #     layout = [
+    #         # Parameters: element_identifier, x_pos, y_pos, width, height, [item properties...]
+    #         # saved layout will be restored automatically
+    #         dashboard.Item("Home", 0, 0, 2, 2, moved=True, resized=True),
+    #     ]
+    #     # saved layout
+    #     # with open("layout.json", "r") as f:
+    #     #     layout = json.load(f)
+
+    #     # If you want to retrieve updated layout values as the user move or resize dashboard items,
+    #     # you can pass a callback to the onLayoutChange event parameter.
+
+    #     def handle_layout_change(updated_layout):
+    #         # You can save the layout in a file, or do anything you want with it.
+    #         # You can pass it back to dashboard.Grid() if you want to restore a saved layout.
+    #         print(updated_layout)
+    #         # save the layout in a file
+    #         with open("layout.json", "w") as f:
+    #             json.dump(updated_layout, f)
+            
+    #         return updated_layout
+
+    #     # Then, pass the layout to the dashboard.Grid() component.
+
+    #     with dashboard.Grid(layout, onLayoutChange=handle_layout_change):
+    #         # Finally, add your dashboard items.
+    #         with mui.Paper(elevation=3, variant="outlined", square=True):
+    #             mui.TextField(
+    #                 label="My text input",
+    #                 defaultValue="Type here",
+    #                 variant="outlined",
+    #             )
+    #             mui.Collapse(in_=True)
+
+            
+
+
+
